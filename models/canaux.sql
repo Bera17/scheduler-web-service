37a64c6a17b7571ad24a14f934c813830bb29d77
Bera17 <43541945+Bera17@users.noreply.github.com>
2021-04-28 06:02:15 +0200

    corr canaux sql => canalId

--- models/canaux.sql
+++ models/canaux.sql
@@ -11,33 +11,33 @@ ALTER TABLE "scheduler-app".canaux
     ADD COLUMN type "scheduler-app".canaltype;
 
 
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-01', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-02', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-03', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-04', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-05', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-06', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-07', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-08', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-09', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-10', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-11', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-12', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-13', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-14', 'true', 'Stream');
-INSERT INTO "scheduler-app".canaux(canalid, nom, visible, type)
+INSERT INTO "scheduler-app".canaux("canalId", nom, visible, type)
 	VALUES (DEFAULT, 'A-NOC-15', 'true', 'Stream');
