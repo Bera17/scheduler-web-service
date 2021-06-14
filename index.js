c5793a4aae6a1e179637045643eeff590b303153
Bera17 <43541945+Bera17@users.noreply.github.com>
2021-06-14 17:09:29 +0200

    refactor

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const {PORT} = require('./utils/config')
 const express = require('express')
 const {morgan} = require('./middleware/index')
-const dbRecord = require('./routes/records')
 var cors = require('cors')
 const bodyParser = require('body-parser')
 
@@ -14,19 +13,12 @@ app.use(morgan.logger)
 const db = require("./models");
 db.sequelize.sync()
 
-// app.get('/api/records', dbRecord.getRecords)
-// app.post('/api/records', dbRecord.createRecord)
-// app.put('/api/records', dbRecord.updateRecord)
-// app.delete('/api/records', dbRecord.deleteRecord)
 require('./routes/records.routes')(app);
 
-//app.get('/api/canaux', dbCanaux.getCanaux)
 require('./routes/canaux.routes')(app);
 
-// app.get('/api/metadata', metaData.getMeta)
 require('./routes/metadata.routes')(app);
 
-//app.post('/api/login', user.getLogin);
 require('./routes/auth.routes')(app);
 
 app.use(morgan.errorHandler)
